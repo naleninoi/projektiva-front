dbc77d054b58b6b2cc1ddf67e1284ebf5091c5b0
viktor.kochetov-arkhipov <viktor.kochetov-arkhipov@sms-a.ru>
2026-03-13 16:54:55 +0300

    увеличено кол-во мок объектов

--- src/app/core/services/object.service.ts
+++ src/app/core/services/object.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { delay, map, Observable } from 'rxjs';
 import { TouristObject } from '../../shared/models/object.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ObjectService {
 
+    constructor(private http: HttpClient) {}
+
     getObjects(): Observable<TouristObject[]> {
-        return of(MOCK_OBJECTS);
+        return this.http.get<TouristObject[]>('assets/mocks/objects.json')
+            .pipe(delay(500)); // Имитация задержки сети
+    }
+
+    getObjectById(id: string): Observable<TouristObject | undefined> {
+        return this.http.get<TouristObject[]>('/assets/mocks/objects.json')
+            .pipe(
+                delay(300),
+                map(objects => objects.find(obj => obj.id === id))
+            );
     }
 }
 
